ffa94a2e4a1c26acce97ab797979a2ed54d43e17
Gourav Rathor <56722848+gouravrathor123@users.noreply.github.com>
2020-05-18 13:45:57 +0530

    levelup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,32 @@
-import React from 'react';
+import React,{ Component } from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand,Collapse,NavbarToggler,NavItem,Nav,UncontrolledDropdown} from 'reactstrap';
 import Menu from './components/MenuComponent';
 import './App.css';
+import {DISHES} from './share/dishes';
 
-function App() {
-  return (
-    <div>
+class App extends Component {
+
+  constructor(props){
+    super(props);
+
+    this.state = {
+      dishes: DISHES
+    };
+  }
+
+  render() {
+    return (
+      <div>
         <Navbar dark color="primary">
-        <div className="container">
-        <NavbarBrand href="/">About Home Produts </NavbarBrand>
-        </div>
+          <div className="container">
+            <NavbarBrand href="/">About Home Produts </NavbarBrand>
+          </div>
         </Navbar>
-        <Menu />
-    </div>
+        <Menu dishes={this.state.dishes} />
+      </div>
   );
+  }
 }
 
 export default App;
